5e15eb9d2b426e0ade5771442c157af39fa3eed0
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-11-03 12:13:06 +0700

    membuat POST utk menmabah data

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 const express = require('express');
 const app = express();
 const PORT = 3000;
@@ -21,72 +19,3 @@ app.post("/music", async (req, res) => {
 });
 
 
-app.get("/music", async (req, res) => {
-  try {
-    const music = await db.Music.findAll();
-    res.status(200).send(music);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send({ message: "Gagal mengambil data", error: err.message });
-  }
-});
-
-
-app.get("/music/:id", async (req, res) => {
-  const id = req.params.id;
-  try {
-    const music = await db.Music.findByPk(id);
-    if (!music) {
-      return res.status(404).send({ message: "Music tidak ditemukan" });
-    }
-    res.status(200).send(music);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send({ message: "Gagal mengambil data", error: err.message });
-  }
-});
-
-app.put("/music/:id", async (req, res) => {
-  const id = req.params.id;
-  const data = req.body;
-
-  try {
-    const music = await db.Music.findByPk(id);
-    if (!music) {
-      return res.status(404).send({ message: "Music tidak tersedia" });
-    }
-
-    await music.update(data);
-    res.status(200).send({ message: "Music berhasil diupdate", music });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send({ message: "Gagal mengupdate data", error: err.message });
-  }
-});
-
-app.delete("/music/:id", async (req, res) => {
-  const id = req.params.id;
-
-  try {
-    const music = await db.Music.findByPk(id);
-    if (!music) {
-      return res.status(404).send({ message: "Music tidak tersedia" });
-    }
-
-    await music.destroy();
-    res.status(200).send({ message: "Music berhasil dihapus" });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send({ message: "Gagal menghapus data", error: err.message });
-  }
-});
-
-db.sequelize.sync()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server started on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error('Gagal konek ke database:', err);
-  });
